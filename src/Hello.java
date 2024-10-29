cc0fafee772dfda2c93599020e920dc1557f8fe0
why <1264036992@qq.com>
2024-10-29 19:18:40 +0800

    new test

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
+// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
+import java.util.Scanner;
+public  class Hello{
+    public static  void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("hello world");
+    }
+}
